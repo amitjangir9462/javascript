23ac6139f4ec295ad771cee6039f86f2b4a4fd8f
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 11:30:57 +0000

    num

---
+++ 01_basics/numandmath.js
@@ -0,0 +1,23 @@
+// let newa = new Number(100)
+// console.log(newa)
+
+// const number1 = 12.3445
+// console.log(number1.toPrecision(1))
+
+// const number1 = 1000000
+// console.log(number1.toLocaleString('en-IN'))
+
+
+// console.log(Math)
+// console.log(Math.abs(-10))
+
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+// console.log(Math.round(4.6))
+
+
+// console.log(Math.random()*10 + 1)
+
+const max = 20
+const min = 10
+// console.log(Math.floor(Math.random()*(max-min+1))+min)
