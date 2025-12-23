71074b9d4eae41c2b82e7f39dc77f35675a827c0
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 08:09:15 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+// let string1 = "123"
+// let string1 = "12fghn"
+
+// let string1 = true
+
+// let string1 = "gh"   +=> NaN
+
+// let string2 = Number(string1)
+// console.log(string1)
+// console.log(typeof string2)
+// console.log(string2)
+
+
+// let number1 = 22
+// let number2 = String(number1)
+// console.log(typeof number2)
+// console.log(number2)
+
+
+let boolean1 = "amit"
+
+let boolean2 = Boolean(boolean1)
+console.log(boolean2)
+console.log(typeof boolean2)
+
+
