d75b9685075626beba10b8c269dd925141ef49f0
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 05:50:48 +0000

    javascript test variables datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //newer version
+
+// alert("Hi trump")
+
+let number = 4
+let string = "hii"
+let boolean = false
+let account = null  // typeof null -> object
+let username // typeof undefined -> undefined
+
+//symbol
+//bigInt
+ 
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof account)
+console.log(typeof username)
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 18655
+let accountEmail = "amit@gmail.com"
+var accountPassword ="amitjangir"
+currentCity = "Jhunjhunu"
+
+let resetPassword
+
+// console.log(accountId)
+
+accountEmail = "aj@gmail.com"
+accountPassword="amit"
+currentCity = "Jaipur"
+
+console.table([accountId,accountEmail,accountPassword,currentCity,resetPassword])
+
