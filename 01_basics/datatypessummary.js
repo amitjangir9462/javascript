e07753eef5edc278aca5a15a82beb9f98de3b580
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 10:01:47 +0000

    datatypessummary

---
+++ 01_basics/datatypessummary.js
@@ -0,0 +1,35 @@
+/* 
+Primitive datatypes
+
+   7 types : Number , String, Boolean , undefined , null , symbol , BigInt
+
+   let number = 12
+   let str = "amit"
+   let boolean = true
+   let undefined ;
+   let value = null
+   let bigInt = 234567845678n
+   
+   let id = symbol("123")
+   let accountId = symbol("123")
+   console.log(id == accountId) => not equal
+
+
+Reference ( Non Primitive datatypes)
+
+  Array , Objects , function
+
+
+  let array = ["amit","jangir"]
+
+  let objects = {
+   name : "amit"
+   age : 22
+  }
+
+
+  const myfunction = function(){
+  console.log("hi")
+  }
+  console.log(myfunction)
+*/
