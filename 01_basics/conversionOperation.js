80843af00ff40ce37c34aa721c4dcd67d55c01c3
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 09:21:43 +0000

    comparision

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -17,10 +17,44 @@
 // console.log(number2)
 
 
-let boolean1 = "amit"
+// let boolean1 = "amit"
 
-let boolean2 = Boolean(boolean1)
-console.log(boolean2)
-console.log(typeof boolean2)
+// let boolean2 = Boolean(boolean1)
+// console.log(boolean2)
+// console.log(typeof boolean2)
 
 
+
+/* ################################  Operations   ####################### */
+
+// let str1 = "Amit"
+// let str2 = " Jangir"
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+
+// let num1 = 1
+// let num2 = 2
+// let num3 = num1 + num2
+// console.log(num3)
+
+
+// console.log("1"+ 2)
+// console.log(1+"2")
+// console.log(1 + 2 + "4")
+// console.log("1"+ 2+2)
+
+// console.log(true)  => true
+// console.log(+true)  => 1
+// console.log(+false)  => 0
+
+
+// let num1 = 1
+// let num2 = -num1
+// console.log(num2)
+
+
+let counter = 10
+++counter 
+console.log(counter)
