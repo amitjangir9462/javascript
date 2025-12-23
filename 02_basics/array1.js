5d31df7fb6f555ae4a2d394ed85c063a4e4e9a93
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 13:22:13 +0000

    array and object

---
+++ 02_basics/array1.js
@@ -0,0 +1,27 @@
+let arr1 = [1,2,3,4]
+let arr2 = [5,6,7,8]
+// arr1.push(arr2)
+// console.log(arr1)  // => array ke andar array 
+
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3)
+
+
+// let arr3= [...arr1,...arr2]  // spread operater
+// console.log(arr3)
+
+
+// let array1 = [1,2,3,[2,3,4,5],7,[1,2,4,6,[7,8]]]
+// let array2 = array1.flat(Infinity)
+// console.log(array2)
+
+
+// console.log(Array.isArray("Amit"))
+// console.log(Array.from("Amit"))
+// console.log(Array.from({name : "Amit"}))
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3))
