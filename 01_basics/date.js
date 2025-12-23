ab93136cb63aaad797784a3da0c2de1204eae714
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 11:56:36 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,10 @@
+// let myDate = new Date()
+// console.log(myDate)
+
+// let myDate = new Date("4-12-2025")
+// console.log(myDate.toLocaleString())
+
+// let timeStamp = Date.now()
+// console.log(timeStamp)
+
+console.log(Math.random(Date.now()))
