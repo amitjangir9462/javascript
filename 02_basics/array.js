24af410127f749364cd1d80828eb7c1fd3a59759
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-23 13:00:49 +0000

    array1

---
+++ 02_basics/array.js
@@ -0,0 +1,31 @@
+let array1 = [1,2,3,4]
+// console.log(typeof array1)
+
+// let array2 = new Array(1,2,3,4)
+// console.log(array2)
+
+// array1.push(5)
+ // array1.pop()
+
+// array1.unshift(0)
+// array1.shift()
+
+
+
+// let newArray = array1.join() 
+// console.log(newArray)
+
+
+
+// Slice , Splice
+
+let arr = [1,2,3,4,5,6]
+console.log(arr)
+
+let arr1 = arr.slice(1,3)
+console.log(arr)
+console.log(arr1)
+
+let arr2 = arr.splice(1,3)
+console.log(arr)
+console.log(arr2)
