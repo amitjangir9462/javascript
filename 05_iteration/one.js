0783997cdd7a10e08632eb32b9422704c885c78e
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-24 12:51:32 +0000

    for loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,46 @@
+//loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i
+//     console.log(element)
+    
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop : ${i}`)
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`Inner loop : ${j}`)
+        
+//     }
+    
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop : ${i}`);
+    
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+    
+// }
+
+
+// const myarray = ["Amit","Jangir","EJ"]
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element)
+    
+// }
+
+
+
+// for (let i = 0; i <=20; i++) {
+//     if(i==5){
+//         console.log("Detected 5")
+//         // break
+//         continue
+//     }
+//     console.log(i)
+    
+// }
