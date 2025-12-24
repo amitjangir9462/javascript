6aae64ebcfc4b7c6e124a620b2760dbd5426905b
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-24 13:06:52 +0000

    while doWhile loops

---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+//while loop
+
+// let i =2
+// while (i<=20) {
+//     console.log(i)
+//     i = i + 2
+    
+// }
+
+
+// let myarr = ["Amit","Jangir","EJ"]
+// let arr = 0
+// while (arr < myarr.length) {
+//     console.log(`${myarr[arr]}`)
+//     arr = arr + 1
+    
+// }
+
+
+// let score = 1
+// do {
+//     console.log(score);
+//     score++
+    
+// } while (score <10);
