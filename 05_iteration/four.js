747e663bed81f3b6012e9bac5e817fea48227e0c
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-24 13:48:59 +0000

    for in

---
+++ 05_iteration/four.js
@@ -0,0 +1,27 @@
+// for in loop
+
+// const myObj = {
+//     js : "Javascript",
+//     cpp : "c++",
+//     python : "python"
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} for ${myObj[key]}`)
+
+// }
+
+
+
+
+// const myArr = ["js","cpp","python","java"]
+// for (const key in myArr) {
+
+//     console.log(`${key} for ${myArr[key]}`)
+    
+    
+// }
+
+
+
+// Notes :- map iterable nhi hai isliye forin loop nhi lagega 
