3f112a64cc672861fb9d7a5dec3652551f15bdec
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-24 09:55:26 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,12 @@
+//Immediate Invoked Function Expressions (IIFE)
+
+
+(function hello(){
+    // named iife
+    console.log(`hii EJ`)
+
+})();
+
+((name) =>{
+    console.log(`hii ${name}`)
+})("EJ")
