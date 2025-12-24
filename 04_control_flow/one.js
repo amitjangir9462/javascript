9f7abbb22b04a953cc92d452f6c29fb17b6474eb
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-24 10:55:56 +0000

    if and else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+//if
+
+// const temp = 41
+// if(temp ==41){
+//     console.log("Okay good"    )
+// }
+// console.log("temp is good")
+
+
+// const temp = 41
+// if(temp >50){
+//     console.log("greater than 50"    )
+// }
+// console.log("Hot")
+
+
+
+// const score =200
+// if(score ==200){
+//     let power = "Fly"
+//     console.log(`power : ${power}`)
+// }
+
+// // console.log(`${power}`)   //power is not defined
