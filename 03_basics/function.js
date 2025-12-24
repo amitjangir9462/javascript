15282781a0932165679a6e5150dc7a9f7fddba3c
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-24 06:21:47 +0000

    function

---
+++ 03_basics/function.js
@@ -0,0 +1,93 @@
+// function code(){
+//     console.log("Hi Amit")
+// }
+// code()
+
+
+// function sum(num1,num2){
+//     console.log(num1 + num2)
+// }
+// const result = sum(3,2)
+// console.log("result: ",result)  // output is undefined
+
+
+// function sum(num1,num2){
+//     let result = num1 + num2
+//     return result 
+// }
+// const result = sum(3,2)
+// console.log("result: ",result)
+
+
+// function name(username){
+
+//     return `My name is ${username} `
+
+// }
+// console.log(name("Amit"))
+
+
+// function name(username){
+//     if(!username){
+//         console.log("Please enter username ")
+//         return
+//     }
+//     return `My name is ${username} `
+
+// }
+// console.log(name("Amit Jangir"))
+
+
+// function addToCart(num1){
+
+//     return num1
+
+// }
+// console.log(addToCart(100,200))    // output is 100
+
+
+// function addToCart(...num1){
+
+//     return num1
+
+// }
+// console.log(addToCart(100,200,300))    output is [100,200,300] in Array
+
+
+
+// function addToCart(val1,val2,...num1){
+
+//     return num1
+
+// }
+// console.log(addToCart(100,200,300,400))   // output is [300,400] because val1 = 100, val2 = 200
+
+
+// const object1 = {
+//     name : "Amit",
+//     price : 2000
+// }
+
+
+// function print(anyObject){
+
+//      return `My name is ${anyObject.name} and my wallet price is ${anyObject.price}`
+// }
+// console.log(print(object1))
+
+
+
+// const array1 =[100,200,300]
+// function secondVale(anyArray){
+//     return anyArray[1]
+
+// }
+// console.log(secondVale(array1))
+
+
+
+
+// function secondVale(anyArray){
+//     return anyArray[1]
+// }
+// console.log(secondVale([100,200,300]))
