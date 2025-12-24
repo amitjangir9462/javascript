7bf3ff5e40d473dffdefb1f29ace37fb5f30e966
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-24 13:34:54 +0000

    forOf

---
+++ 05_iteration/three.js
@@ -0,0 +1,43 @@
+// for of
+
+// let mrArr = [ 1,2,3,4,5]
+// for (const num of mrArr) {
+//     console.log(num)
+    
+// }
+
+
+// let myStr = "Amit Jangir"
+// for (const str of myStr) {
+//     console.log(str)
+    
+// }
+
+
+
+//Map
+
+// const map = new Map()
+
+// map.set("IN" ,"Indian")
+// map.set("USA","United state of america")
+// map.set("UK","United Kingdom")
+
+// // console.log(map)
+
+// // for (const key of map) {    //map unique and same ordered key , value deta hai
+// for (const [key , value] of map){
+//     console.log(key,value)
+    
+// }
+
+
+// const myObj = {
+//     "name" : "Amit",
+//     "village":"khetri"
+// }
+
+// for (const [key,value] of myObj) {         //myObj is not iterable
+//     console.log(key,value)
+    
+// }
