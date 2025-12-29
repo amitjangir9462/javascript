6cb7bad7e3819a293785726e3e97090e314e18db
amitjangir9462 <amitjangir9462@gmail.com>
2025-12-29 05:00:34 +0000

    math pi

---
+++ 10_classes_oops/mathpi.js
@@ -0,0 +1,27 @@
+// console.log(Math.PI)
+// Math.PI = 5
+// console.log(Math.PI);
+
+const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(descripter)
+
+const chai= {
+    name : 'ginger chai',
+    price:250,
+    isAvailable : true 
+
+}
+// console.log(chai)
+console.log(Object.getOwnPropertyDescriptor(chai,'name'))
+// Object.defineProperty(chai,'name',{
+//     writable : false,
+//     enumerable : false
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai,'name'))
+
+
+for (let [key,value] of Object.entries(chai)) {
+    console.log(`${key} : ${value}`)
+    
+}
